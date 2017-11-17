a6ac3b2147910a0c28bd300531ec95d32f6969ca
birkl <lkbirkl@googlemail.com>
2017-11-17 13:19:38 +0100

    Made a first draft of the Connect() function. Gonna finish it as soon as I got access to Visual Studio.

--- Assets/Scripts/Connection.cs
+++ Assets/Scripts/Connection.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mono.Data.Sqlite;
-using System.Data;
-using System;
 
 public class Connection : MonoBehaviour
 {
@@ -20,6 +17,30 @@ public class Connection : MonoBehaviour
     {
         //Calculate, which corners are closest together
         //Draws a line between those 2 corners
+
+		//first: get midpoint between two nodes
+		//for each node check which corner is closest
+		//connect those two corners
+
+		Vector3 midPoint = data.nodeA.gameObject.transform.position - data.nodeB.gameObject.transform.position; //maybe rework
+		float shortestDistance = float.MaxValue;
+		Vector3[] connectionPoints = new Vector3[2];
+		
+		//check all corners of NodeA (save in connectionPoints[0])
+		
+		shortestDistance = float.MaxValue;
+		
+		//check all corners of NodeB (save in connectionPoints[1])
+		
+		//calculate midpoint between cornerA and CornerB
+		midPoint = connectionPoints[0] - connectionPoints[1]; //maybe rework
+		transform.position = midPoint;
+		//rotate towards connectionPoints[0]
+		float angle = Mathf.Atan2(connectionPoints[0].x, connectionPoints[0].y) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+		//rescale
+		float scale = (midPoint - connectionPoints[0]).Distance;
+		gameObject.transform.localScale = scale;
     }
 
     private void OnMouseOver()
